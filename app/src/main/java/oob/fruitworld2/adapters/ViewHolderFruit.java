9373a94b4a306c74743c535881a56cb399e4bda5
Julio López Torres <juliolopeztorres@gmail.com>
2017-06-27 19:50:56 +0200

    feat: Add listener definition in order to fetch clicks in fruit's background image

--- app/src/main/java/oob/fruitworld2/adapters/ViewHolderFruit.java
+++ app/src/main/java/oob/fruitworld2/adapters/ViewHolderFruit.java
@@ -1,12 +1,15 @@
 package oob.fruitworld2.adapters;
 
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import oob.fruitworld2.R;
+import oob.fruitworld2.Utils.Utils;
 import oob.fruitworld2.models.Fruit;
 
 class ViewHolderFruit extends RecyclerView.ViewHolder {
@@ -26,10 +29,28 @@ class ViewHolderFruit extends RecyclerView.ViewHolder {
         this.fruitAmount = (TextView) itemView.findViewById(R.id.fruitAmount);
     }
 
-    void bind(final Fruit fruit, Activity activity) {
+    void bind(final Fruit fruit, Activity activity, final RecyclerViewAdapter.onBackgroundClick listener) {
         Picasso.with(activity).load(fruit.getBackground()).fit().into(this.fruitBackground);
+        this.fruitBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(fruit, getAdapterPosition());
+            }
+        });
+
         this.fruitName.setText(fruit.getName());
         this.fruitDescription.setText(fruit.getDescription());
-        this.fruitAmount.setText(String.valueOf(fruit.getAmount()));
+
+        int amount = fruit.getAmount();
+
+        if (amount == Utils.MAXIMUM_AMOUNT) {
+            this.fruitAmount.setTextColor(ContextCompat.getColor(activity, android.R.color.holo_red_dark));
+            this.fruitAmount.setTypeface(null, Typeface.BOLD);
+        } else {
+            this.fruitAmount.setTextColor(ContextCompat.getColor(activity, android.R.color.holo_blue_light));
+            this.fruitAmount.setTypeface(null, Typeface.NORMAL);
+        }
+
+        this.fruitAmount.setText(String.valueOf(amount));
     }
 }
